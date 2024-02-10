ddeeb5ccac6738738d49cb93507c699093200ed4
Shestakov Viktor <phd.shestakov@gmail.com>
2024-02-10 03:12:05 +0300

    Fix

--- src/editor.ts
+++ src/editor.ts
@@ -5,7 +5,7 @@ import "@style/global.scss";
 import {Viewer} from "@viewer";
 import {Core} from "@core";
 import {UI} from "@ui";
-import {AssetSelectDialog} from "./ui/feature/asset/component";
+// import {AssetSelectDialog} from "./ui/feature/asset/component";
 
 const root = document.createElement("div");
 root.id = "root";
@@ -15,11 +15,11 @@ const viewer = new Viewer(root);
 const core = new Core(viewer);
 const ui = new UI(root, viewer, core);
 
-fetch("/project.mp")
-    .then((response) => response.blob())
-    .then(file => core.importProject(file))
-    .then(() => ui.context.modal.show(AssetSelectDialog()))
-    .catch(err => {throw new Error("Project init error: " + err);});
+// fetch("/project.mp")
+//     .then((response) => response.blob())
+//     .then(file => core.importProject(file))
+//     .then(() => ui.context.modal.show(AssetSelectDialog()))
+//     .catch(err => {throw new Error("Project init error: " + err);});
 
-// ui.showInitialDialog();
+ui.showInitialDialog();
 
