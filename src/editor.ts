9b25858f904f58f276c32a354ea6346082982f19
Shestakov Viktor <phd.shestakov@gmail.com>
2024-02-10 03:10:19 +0300

    Add asset remove feature

--- src/editor.ts
+++ src/editor.ts
@@ -5,6 +5,7 @@ import "@style/global.scss";
 import {Viewer} from "@viewer";
 import {Core} from "@core";
 import {UI} from "@ui";
+import {AssetSelectDialog} from "./ui/feature/asset/component";
 
 const root = document.createElement("div");
 root.id = "root";
@@ -14,9 +15,11 @@ const viewer = new Viewer(root);
 const core = new Core(viewer);
 const ui = new UI(root, viewer, core);
 
-// fetch("/project.mp")
-//     .then((response) => response.blob())
-//     .then(file => core.importProject(file))
-//     .catch(err => {throw new Error("Project init error: " + err);});
-ui.showInitialDialog();
+fetch("/project.mp")
+    .then((response) => response.blob())
+    .then(file => core.importProject(file))
+    .then(() => ui.context.modal.show(AssetSelectDialog()))
+    .catch(err => {throw new Error("Project init error: " + err);});
+
+// ui.showInitialDialog();
 
